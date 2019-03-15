008411901e19b5330d01e937ab46644b63b64869
Flaturtha <rich@talesofmurder.com>
2019-03-15 15:06:29 -0500

    builds basic components sans functionality

---
+++ src/components/AppPlaylist/AppPlaylist.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AppPlaylist.css';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class AppPlaylist extends React.Component{
+  render(){
+    return(
+      <div className="App-playlist">
+      <SearchResults />
+      <Playlist />
+    </div>
+    )
+  }
+}
+
+export default AppPlaylist;
---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './TrackList.css';
+
+class TrackList extends React.Component{
+  render(){
+    return(
+      <div className="TrackList">
+      <div className="Track">
+        <div className="Track-information">
+          <h3>Stronger</h3>
+          <p>Britney Spears | Oops!... I Did It Again</p>
+        </div>
+        <a className="Track-action">-</a>
+      </div>
+      <div className="Track">
+        <div className="Track-information">
+          <h3>So Emotional</h3>
+          <p>Whitney Houston | Whitney</p>
+        </div>
+        <a className="Track-action">-</a>
+      </div>
+      <div className="Track">
+        <div className="Track-information">
+          <h3>It's Not Right But It's Okay</h3>
+          <p>Whitney Houston | My Love Is Your Love</p>
+        </div>
+        <a className="Track-action">-</a>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default TrackList;
